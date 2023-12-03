762304ff27f26815900e91cd2b40b1d9b1710bc7
Sahil Prajapati <sahilprj@amazon.com>
2023-12-03 12:20:38 +0530

    Single SPA demo Nav app

---
+++ src/root.component.tsx
@@ -0,0 +1,17 @@
+import "./root.component.css";
+import { BrowserRouter, Link } from "react-router-dom";
+
+export default function Root(props) {
+  return (
+    <BrowserRouter>
+      <nav className="nav">
+        <Link to="/page1" className="link">
+          Page 1
+        </Link>
+        <Link to="/page2" className="link">
+          Page 2
+        </Link>
+      </nav>
+    </BrowserRouter>
+  );
+}
